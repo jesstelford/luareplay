4adeeb6306216766a28137b0df63a793f02e95cc
Jess Telford <jess@jes.st>
2013-01-03 19:27:21 +1100

    Refactoring of tests

--- spec/record_spec.lua
+++ spec/record_spec.lua
@@ -3,33 +3,31 @@ require "spec.bootstrap"
 describe('table recording module #record', function()
 
     it('contains record method when created', function()
-
         local object = require "recordable"({})
         assert.has_method(object, 'record')
-
     end)
 
-    it('adds record method when created', function()
-
+    it('adds record method when extending', function()
         local object = {}
         object = require "recordable"(object)
         assert.has_method(object, 'record')
-
     end)
 
     it('modifies the original object', function()
-
         local object = {}
         require "recordable"(object)
         assert.has_method(object, 'record')
-
     end)
 
     it('throws error when record already exists', function()
-        local recordable = require "recordable"
         local object = {record = 1}
-        assert.has_error(function() recordable(object) end)
-
+        assert.has_error(function() require "recordable"(object) end)
     end)
+
+end)
+
+describe('recording modifications to table', function()
+
+
 
 end)
