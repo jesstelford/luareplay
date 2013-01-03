d62370b8a2296fc17ddc5787ece63d27d795a43f
Jess Telford <jess@jes.st>
2013-01-03 18:29:19 +1100

    More tests

--- spec/record_spec.lua
+++ spec/record_spec.lua
@@ -1,9 +1,25 @@
 require "spec.bootstrap"
 
 describe('table recording #record', function()
-    it('contains record method', function()
+    it('contains record method when created', function()
 
         local object = require "recordable"({})
+        assert.has_method(object, 'record')
+
+    end)
+
+    it('adds record method when created', function()
+
+        local object = {}
+        object = require "recordable"(object)
+        assert.has_method(object, 'record')
+
+    end)
+
+    it('modifies the original object', function()
+
+        local object = {}
+        require "recordable"(object)
         assert.has_method(object, 'record')
 
     end)
