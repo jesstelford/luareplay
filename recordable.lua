88283cdae2709f36dcb0c41f4a3c414f8a531a01
Jess Telford <jess@jes.st>
2013-01-04 18:36:47 +1100

    Accept whitelist of params to serialize

--- recordable.lua
+++ recordable.lua
@@ -1,11 +1,20 @@
 
-return (function(me, serpent)
+return (function(me, serpent, whitelist)
     
     assert(serpent.dump, 'serpent library must be present')
+    assert(whitelist == nil or type(whitelist) == 'table', 'whitelist must be a table of properties to record')
     assert(me.serialize == nil, "'record' method already exists on " .. tostring(me))
 
+    local serpentOptions = {}
+
+    -- only set whitelist if options passed in
+    for k,v in pairs(whitelist or {}) do
+        serpentOptions.keyallow = whitelist
+        break
+    end
+
     me.serialize = function()
-        return serialize(me)
+        return serpent(me, serpentOptions)
     end
 
     return me
