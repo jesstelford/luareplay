4e854a087e047d3ea8fc2a937755ebf9d6cdb923
Jess Telford <jess@jes.st>
2013-01-04 18:49:08 +1100

    Tests around whitelisting params to be serialized

--- spec/record_spec.lua
+++ spec/record_spec.lua
@@ -26,6 +26,22 @@ describe('table recording module #record', function()
         assert.has_error(function() require "recordable"(object, serpent) end)
     end)
 
+    it('throws error when whitelist not table', function()
+        assert.has_error(function() require "recordable"({}, serpent, function() end) end)
+        assert.has_error(function() require "recordable"({}, serpent, "foo") end)
+        assert.has_error(function() require "recordable"({}, serpent, true) end)
+    end)
+
+    it('accepts empty whitelist table', function()
+        local object = {}
+        assert.has_no.errors(function() require "recordable"(object, serpent, {}) end)
+    end)
+
+    it('accepts whitelist table', function()
+        local object = {}
+        assert.has_no.errors(function() require "recordable"(object, serpent, {x = true}) end)
+    end)
+
 end)
 
 describe('recording modifications to table', function()
